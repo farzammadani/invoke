c80dffeec48f3475e67484a3204f14069bf93a95
Farzam Madani <farzammdn@gmail.com>
2025-05-14 21:05:44 +0200

    yaml parses test. also add url parsing to code. then write test with test yaml files.

--- tests/tests/integration/YamlJobParserTest.php
+++ tests/tests/integration/YamlJobParserTest.php
@@ -1,9 +1,8 @@
 <?php
-namespace App\Tests\tests\integration;
-/**
- * @group kiwis
- */
 
+namespace App\Tests\tests\integration;
+
+use App\Context\JobSystem\Domain\Job;
 use App\Context\JobSystem\Infrastructure\YamlJobParser;
 use App\Tests\Support\IntegrationTestCase;
 
@@ -17,5 +16,13 @@ class YamlJobParserTest extends IntegrationTestCase
          */
         $yamlParser = $this->tester->grabService(YamlJobParser::class);
         $parseResult = $yamlParser->parse();
+        /**
+         * @var Job[] $jobs
+         */
+        $jobs = $parseResult->jobs;
+        $firstJob = $jobs[0];
+        $this->assertEquals($firstJob->name->value, 'Notify');
+        $this->assertEquals($firstJob->schedule->value, '0 2 * * *');
+        $this->assertEquals($firstJob->enabled->value, true);
     }
 }
